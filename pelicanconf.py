24784204d14e4b3c95027f85982df2ab9388df92
Alessandro Molina <alessandro@molina.fyi>
2025-05-30 19:38:57 +0200

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,7 @@ NAME = "Alessandro Molina"
 WEBSITE = "alessandro.molina.fyi"
 LINKEDIN = "amol0"
 GITHUB = "amol-"
-TWITTER = "__amol__"
+TWITTER = "@_amol_"
 MEDIUM = "@__amol__"
 
 # Feed generation is usually not desired when developing
